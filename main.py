bd73ffdbb2ca509d0988f2e0960bb3a0cca83b1b
Dheeraj <dlovej009@gmail.com>
2023-06-16 16:41:16 +0530

    dj

--- main.py
+++ main.py
@@ -5,28 +5,38 @@ user_info_=cl.user_by_username_v1("dheeraj_joshi2006")
 print(user_info_)
 app = Flask(__name__)
 
-@app.route('/user_info', methods=['POST'])
+@app.route('/user_info', methods=['GET','POST'])
 def user_info():
-    username = request.form.get('username')
-    # Process username data
-    user_info_=cl.user_by_username_v1(username)
-    return user_info_
+    if request.method=='Post':
+        username = request.form.get('username')
+        # Process username data
+        user_info_=cl.user_by_username_v1(username)
+        return user_info_
+    else:
+           
+        return "this is fin"
 
-@app.route('/user_medias', methods=['POST'])
+@app.route('/user_medias', methods=['GET','POST'])
 def user_medisa():
-    user_id = request.form.get('user_id')
-    # Process user_id data
-    user_medias_=cl.user_medias(user_id)
-    print(user_medias_)
-    return user_medias_
+    if request.method=='Post':
+        user_id = request.form.get('user_id')
+        # Process user_id data
+        user_medias_=cl.user_medias(user_id)
+        print(user_medias_)
+        return user_medias_
+    else:
+        return "this is fin"
 
-@app.route('/user_stories', methods=['POST'])
+@app.route('/user_stories', methods=['GET','POST'])
 def user_storied():
-    username = request.form.get('username')
-    # Process user_id data
-    user_stories=cl.user_stories_by_username_v1(username)
-    
-    return user_stories
+    if request.method=='Post':
+        username = request.form.get('username')
+        # Process user_id data
+        user_stories=cl.user_stories_by_username_v1(username)
+        
+        return user_stories
+    else:
+        return "this is fin"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
